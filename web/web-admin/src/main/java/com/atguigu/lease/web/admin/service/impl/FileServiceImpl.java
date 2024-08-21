9b4bc7d3e6be8f9728c1b2f4d1b325c46146a7a0
yejiaqi <1095383905@qq.com>
2024-08-21 15:04:07 +0800

    feat(master): 文件上传异常处理

--- web/web-admin/src/main/java/com/atguigu/lease/web/admin/service/impl/FileServiceImpl.java
+++ web/web-admin/src/main/java/com/atguigu/lease/web/admin/service/impl/FileServiceImpl.java
@@ -3,10 +3,14 @@ package com.atguigu.lease.web.admin.service.impl;
 import com.atguigu.lease.common.minio.MinioProperties;
 import com.atguigu.lease.web.admin.service.FileService;
 import io.minio.*;
+import io.minio.errors.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.UUID;
@@ -21,35 +25,30 @@ public class FileServiceImpl implements FileService {
     private MinioProperties properties;
 
     @Override
-    public String upload(MultipartFile file) {
+    public String upload(MultipartFile file) throws ServerException, InsufficientDataException, ErrorResponseException, IOException, NoSuchAlgorithmException, InvalidKeyException, InvalidResponseException, XmlParserException, InternalException {
 
-        try {
-            boolean bucketExists = minioClient.bucketExists(BucketExistsArgs.builder()
+        boolean bucketExists = minioClient.bucketExists(BucketExistsArgs.builder()
+                .bucket(properties.getBucketName())
+                .build());
+        if (!bucketExists) {
+            minioClient.makeBucket(MakeBucketArgs.builder()
                     .bucket(properties.getBucketName())
                     .build());
-            if (!bucketExists) {
-                minioClient.makeBucket(MakeBucketArgs.builder()
-                        .bucket(properties.getBucketName())
-                        .build());
-                minioClient.setBucketPolicy(SetBucketPolicyArgs.builder()
-                        .bucket(properties.getBucketName())
-                        .config(createBucketPolicyConfig(properties.getBucketName()))
-                        .build());
-            }
-            String filename = new SimpleDateFormat("yyyyMMdd").format(new Date()) + "/" + UUID.randomUUID() + "-" + file.getOriginalFilename();
-            minioClient.putObject(PutObjectArgs.builder()
+            minioClient.setBucketPolicy(SetBucketPolicyArgs.builder()
                     .bucket(properties.getBucketName())
-                    .stream(file.getInputStream(), file.getSize(), -1)
-                    .object(filename)
-                    .contentType(file.getContentType())
+                    .config(createBucketPolicyConfig(properties.getBucketName()))
                     .build());
-
-            return String.join("/", properties.getEndpoint(), properties.getBucketName(), filename);
-
-        } catch (Exception e) {
-            e.printStackTrace();
         }
-        return null;
+        String filename = new SimpleDateFormat("yyyyMMdd").format(new Date()) + "/" + UUID.randomUUID() + "-" + file.getOriginalFilename();
+        minioClient.putObject(PutObjectArgs.builder()
+                .bucket(properties.getBucketName())
+                .stream(file.getInputStream(), file.getSize(), -1)
+                .object(filename)
+                .contentType(file.getContentType())
+                .build());
+
+        return String.join("/", properties.getEndpoint(), properties.getBucketName(), filename);
+
 
     }
     private String createBucketPolicyConfig(String bucketName) {
